ecec7224862dcfedcf99e2dd1969b62acbb431af
Olta <olta.petriti13@gmail.com>
2019-08-12 17:01:50 +0200

    Updates

---
+++ precache-manifest.a2743210ed248e3281f981991df50edc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "764e9d2332f97ad3b7a7a5296ef17e05",
+    "url": "/ravenous-project/index.html"
+  },
+  {
+    "revision": "7fee1249ac0a77ebe6e2",
+    "url": "/ravenous-project/static/css/main.1fd37934.chunk.css"
+  },
+  {
+    "revision": "bca943ab24bdfcd15706",
+    "url": "/ravenous-project/static/js/2.f0927397.chunk.js"
+  },
+  {
+    "revision": "7fee1249ac0a77ebe6e2",
+    "url": "/ravenous-project/static/js/main.25714ec1.chunk.js"
+  },
+  {
+    "revision": "51191d4ad184f324692e",
+    "url": "/ravenous-project/static/js/runtime~main.57c0fe2f.js"
+  }
+]);
